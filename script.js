2b8774e1117c8c5beac66183cf6e7267a621bc4c
amang007 <amangupta.ag36@gmail.com>
2021-01-24 21:30:04 +0530

    find Biggest Fraction

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 var date = new Date();
 	document.body.innerHTML = "<h1>Today is:" + date + "</h1>";
+
+function findBiggestFraction(a, b) {
+	var result;
+	a > b ? result = ["firstFraction", a] :  result = ["secondFraction", b];
+	return result;
+}
+
+var firstFraction = 60/3;
+var secondFraction = 55/3;
+
+var fractionResult = findBiggestFraction(firstFraction, secondFraction);
+
+console.log("First Fraction Result: ", firstFraction);
+console.log("Second Fraction Result: ", secondFraction);
+console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
